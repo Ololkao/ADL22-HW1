9f08b38a486512f7760a8ac01f66df2b0ab97149
Ololkao <h24076087@gs.ncku.edu.tw>
2022-10-17 23:51:38 +0800

    update download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://www.cmlab.csie.ntu.edu.tw/~briankao/ADL22/best_intent.pth -O best_intent.pth
+wget https://www.cmlab.csie.ntu.edu.tw/~briankao/ADL22/best_slot.pth -O best_slot.pth
+wget https://www.cmlab.csie.ntu.edu.tw/~briankao/ADL22/cache.zip -O cache.zip && unzip cache.zip
